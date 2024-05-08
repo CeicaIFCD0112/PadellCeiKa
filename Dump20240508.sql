72aea05cb297b8ba6065869915e90fdb70b42b6d
CeicaIFCD0112 <rod.balseiro.ricardo@gmail.com>
2024-05-08 18:49:25 +0200

    new bbdd

--- Dump20240508.sql
+++ Dump20240508.sql
@@ -42,6 +42,33 @@ INSERT INTO `horas` VALUES (1,'10-11'),(2,'11-12'),(3,'12-13'),(4,'13-14'),(5,'1
 UNLOCK TABLES;
 
 --
+-- Table structure for table `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `news` (
+  `idnews` int(11) NOT NULL AUTO_INCREMENT,
+  `news_name` varchar(255) DEFAULT NULL,
+  `news_title` varchar(255) DEFAULT NULL,
+  `news_pic` varchar(255) DEFAULT NULL,
+  `news_descr` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idnews`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `news`
+--
+
+LOCK TABLES `news` WRITE;
+/*!40000 ALTER TABLE `news` DISABLE KEYS */;
+INSERT INTO `news` VALUES (1,'noticia1','¿Cuáles son las claves de una pretemporada de pádel?','img_1.jpg','Cuando cada vez falta menos para el comienzo de la nueva temporada, los jugadores profesionales se encuentran en plena pretemporada. El que fuera fisioterapeuta oficial del circuito, Jordi Riba, dejo algunas claves en una entrevista al World Padel Tour so'),(2,'noticia2','adidas lanza al mercado la nueva Adipower Legend de Seba Nerone','img_2.jgp','¿Cómo es la nueva Adipower Legend?'),(3,'noticia3','Wall Breaker 375, la joya de la corona de Tecnifibre','img_3.jpg','Wall Breaker 375 es una pala de gama alta lanzada por Tecnifibre y que lleva el mejor jugador francés, Ben Tison, así como el recién fichado Fede Chiostri, ex-número 1 del circuito APT Padel Tour (ahora A1 Padel).');
+/*!40000 ALTER TABLE `news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `pista`
 --
 
@@ -165,4 +192,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-08 17:15:10
+-- Dump completed on 2024-05-08 18:48:33
